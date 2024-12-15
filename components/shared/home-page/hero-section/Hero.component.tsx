7f1cc5bbcd401b86dd192c308695ed12611ea4c8
kgs <simonudemy09@gmail.com>
2024-12-15 20:21:12 +0100

    adjusting home page to mobile

--- components/shared/home-page/hero-section/Hero.component.tsx
+++ components/shared/home-page/hero-section/Hero.component.tsx
@@ -5,12 +5,12 @@ export const Hero = () => {
     <div className="relative flex w-full">
         <video
             className="w-full h-full max-h-[80vh] object-cover" 
-            src="https://res.cloudinary.com/dtts0tqni/video/upload/v1733491031/videos/fire2_q8r8mc.mp4" 
+            src="https://res.cloudinary.com/dtts0tqni/video/upload/v1734185913/videos/NOBORRAR.mp4" 
             autoPlay 
             loop 
             muted 
         />
-        <div className="absolute inset-10 flex flex-col gap-10 items-center justify-center bg-black bg-opacity-50">
+        <div className="hidden md:absolute md:inset-10 md:flex flex-col gap-10 items-center justify-center bg-black bg-opacity-50">
             <h1 className="text-white text-8xl font-bold">Videos App</h1>
             <p className="text-white text-2xl font-bold">A place to store your favorite videos</p>
         </div>
