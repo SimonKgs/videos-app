03091b0c2f2000eec97909389829b28baddd41c9
kgs <simonudemy09@gmail.com>
2024-12-15 20:06:51 +0100

    adjusting css + size advise

--- app/(private)/[id]/upload/page.tsx
+++ app/(private)/[id]/upload/page.tsx
@@ -8,8 +8,9 @@ export const metadata: Metadata = {
 
 export default function UploadPage() {
     return (
-        <div className="flex min-h-screen flex-col items-center pt-24">
+        <div className="flex min-h-screen sm:w-1/2 xl:w-1/4 flex-col items-center pt-24">
             <h1 className="text-2xl">Upload Page</h1>
+            <p className="text-gray-500 text-xl">Max size of video is 5000KB</p>
             <UploadVideoForm />
         </div>
     );
